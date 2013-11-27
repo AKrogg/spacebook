cf6a4f294cc5bbba02fe4fa452476c8bac1084f0
Andrew Kroger <andrew@kroger.net.au>
2013-11-28 09:15:25 +1100

    Added statuses, created an application, and fun things.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spacebook::Application.config.secret_token = '8f9cec54f17c59c7d243eff793511f7faba7f6fee6422aff8a43b35e9a4c3655c7591e177fe05ed9c91528ae749f9b313c6c4a958870db88340768cbb41acb04'
